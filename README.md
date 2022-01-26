671c501d186fe44dc453c087993ac5b96ace0993
laurencemachin <laurencemachin1@gmail.com>
2022-01-26 19:00:15 +0000

    Refactoring of code, and turtle now turns

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ In this readMe I'll just be laying out my thoughts as I approach the problem so 
 The first thing I notice from the outline is that the buttons do not work as expected. I'll be sorting that first, as it needs to work intuitively since its an
 educational tool. I believe there should be a forward button, which moves the turtle forward in the direction it is facing, and rotation buttons so that its
 direction can be changed. I might make some adjustments to this based on how it feels.<br>
-Also, it needs to look like a turtle, obviously. So I'll find a turtle sprite and be implementing that.
+Also, it needs to look like a turtle, obviously. So I'll find a turtle sprite and be implementing that.<br><br>
+
+## Wednesday 26th
+I have the buttons working as I intended, and a turtle sprite, however I need to make sure the turtle turns when you press the turn buttons.<br>
+After this I want to add a programmability function, so that the user can press multiple buttons in a sequence, then press a play button and watch the turtle perform the functions they input.
--- main.py
+++ main.py
@@ -1,58 +1,71 @@
-from turtle import turtles
+from turtle import turtles, width
 import numpy as np
 import tkinter as tk
 from PIL import Image,ImageTk
 
+class RobotRoamer(object):
+    def __init__(self, master, canvasSize, turtleSize, **kwargs):
+        self.master = master
+        self.canvasSize = canvasSize
+        self.turtleSize = turtleSize
+        self.direction = 0
+        self.coords = [canvasSize/2, canvasSize/2]
+        self.canvas = tk.Canvas(master, bg="white", width=canvasSize, height=canvasSize)
+        self.canvas.grid(column=0, row=0, columnspan=4)
+
+        turtle = Image.open("images/turtle.png")
+        turtle = turtle.resize((self.turtleSize,self.turtleSize), Image.ANTIALIAS)
+        self.turtleTkImg = ImageTk.PhotoImage(turtle)
+        self.canvas.create_image(self.coords[0]-self.turtleSize/2,self.coords[1]-self.turtleSize/2, anchor=tk.NW, image=self.turtleTkImg)
+
+        tk.Button(window, 
+                  text="↰", 
+                  height=7,
+                  width=10,
+                  command=self.turn_left).grid(column=0, row=1)
+        tk.Button(window, 
+                  text="↑",
+                  height=7,
+                  width=10,
+                  command=self.move_forward).grid(column=1, row=1)
+        tk.Button(window, 
+                  text="↱", 
+                  height=7,
+                  width=10,
+                  command=self.turn_right).grid(column=2, row=1)
+
+
+    def move_forward(self):
+        self.canvas.create_line(self.coords[0], 
+                                self.coords[1], 
+                                self.coords[0] + np.sin(self.direction*(np.pi/180))*self.turtleSize, 
+                                self.coords[1] - np.cos(self.direction*(np.pi/180))*self.turtleSize, fill="black", width=1)
+        self.coords[0] += np.sin(self.direction*(np.pi/180))*self.turtleSize
+        self.coords[1] -= np.cos(self.direction*(np.pi/180))*self.turtleSize
+        self.draw_turtle()
+        #self.canvas.move(turtleImg, np.sin(direction*(np.pi/180))*turtleSize, -np.cos(direction*(np.pi/180))*turtleSize)
+
+    def turn_left(self):    
+        self.direction -= 90
+        self.draw_turtle()
+
+    def turn_right(self):
+        self.direction += 90
+        self.draw_turtle()
+        
+    def draw_turtle(self):
+        turtle = Image.open("images/turtle.png")
+        turtle = turtle.resize((self.turtleSize,self.turtleSize), Image.ANTIALIAS)
+        self.turtleTkImg = ImageTk.PhotoImage(turtle.rotate(-self.direction))
+        self.canvas.create_image(self.coords[0]-self.turtleSize/2,self.coords[1]-self.turtleSize/2, anchor=tk.NW, image=self.turtleTkImg)
+
+
 window = tk.Tk()
+robotRoamer = RobotRoamer(window, 500, 40)
 
-coords = [250, 250]
-
-#in degrees from forward (+ve clockwise)
-direction = 0
-
-canvas = tk.Canvas(window, bg="white", height=500, width=500)
-canvas.grid(column=0, row=0, columnspan=4)
-
-# image = Image.open(Image_Location)
-# image = image.resize((250, 250), Image.ANTIALIAS) ## The (250, 250) is (height, width)
-# self.pw.pic = ImageTk.PhotoImage(image)
-
-turtleSize = 40
-turtle = Image.open("images/turtle.png")
-turtle = turtle.resize((turtleSize,turtleSize), Image.ANTIALIAS)
-turtle = ImageTk.PhotoImage(turtle)
-turtleImg = canvas.create_image(250-turtleSize/2,250-turtleSize/2, anchor=tk.NW, image=turtle)
-
-
-def move_forward():
-    canvas.create_line(coords[0], coords[1], coords[0] + np.sin(direction*(np.pi/180))*turtleSize, coords[1] - np.cos(direction*(np.pi/180))*turtleSize, fill="black", width=1)
-    coords[0] += np.sin(direction*(np.pi/180))*turtleSize
-    coords[1] -= np.cos(direction*(np.pi/180))*turtleSize
-    canvas.move(turtleImg, np.sin(direction*(np.pi/180))*turtleSize, -np.cos(direction*(np.pi/180))*turtleSize)
-
-def turn_left():
-    global direction
-    direction -= 90
-
-def turn_right():
-    global direction
-    direction += 90
-
-tk.Button(window, 
-          text="↰", 
-          height=7,
-          width=10,
-          command=turn_left).grid(column=0, row=1)
-tk.Button(window, 
-          text="↑",
-          height=7,
-          width=10,
-          command=move_forward).grid(column=1, row=1)
-tk.Button(window, 
-          text="↱", 
-          height=7,
-          width=10,
-          command=turn_right).grid(column=2, row=1)
-
+# turtle = Image.open("images/turtle.png")
+# turtle = turtle.resize((40,40), Image.ANTIALIAS)
+# turtleTkImg = ImageTk.PhotoImage(turtle)
+# robotRoamer.canvas.create_image(250,250, anchor=tk.NW, image=turtleTkImg)
 
 window.mainloop()
