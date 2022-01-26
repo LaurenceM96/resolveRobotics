e9db3e2098773ca270f7f74365f2cfd862ddbd28
laurencemachin <laurencemachin1@gmail.com>
2022-01-26 17:52:01 +0000

    Changed button functionality & added turtle sprite

--- main.py
+++ main.py
@@ -1,44 +1,58 @@
+from turtle import turtles
+import numpy as np
 import tkinter as tk
+from PIL import Image,ImageTk
 
 window = tk.Tk()
 
-coords = [150, 150]
+coords = [250, 250]
 
-canvas = tk.Canvas(window, bg="white", height=300, width=300)
+#in degrees from forward (+ve clockwise)
+direction = 0
+
+canvas = tk.Canvas(window, bg="white", height=500, width=500)
 canvas.grid(column=0, row=0, columnspan=4)
 
+# image = Image.open(Image_Location)
+# image = image.resize((250, 250), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+# self.pw.pic = ImageTk.PhotoImage(image)
 
-def move_up():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] + 10, fill="black", width=1)
-    coords[1] += 10
-
-
-def move_down():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] - 10, fill="black", width=1)
-    coords[1] -= 10
-
-
-def move_left():
-    canvas.create_line(coords[0], coords[1], coords[0] - 10, coords[1], fill="black", width=1)
-    coords[0] -= 10
+turtleSize = 40
+turtle = Image.open("images/turtle.png")
+turtle = turtle.resize((turtleSize,turtleSize), Image.ANTIALIAS)
+turtle = ImageTk.PhotoImage(turtle)
+turtleImg = canvas.create_image(250-turtleSize/2,250-turtleSize/2, anchor=tk.NW, image=turtle)
 
 
-def move_right():
-    canvas.create_line(coords[0], coords[1], coords[0] + 10, coords[1], fill="black", width=1)
-    coords[0] += 10
+def move_forward():
+    canvas.create_line(coords[0], coords[1], coords[0] + np.sin(direction*(np.pi/180))*turtleSize, coords[1] - np.cos(direction*(np.pi/180))*turtleSize, fill="black", width=1)
+    coords[0] += np.sin(direction*(np.pi/180))*turtleSize
+    coords[1] -= np.cos(direction*(np.pi/180))*turtleSize
+    canvas.move(turtleImg, np.sin(direction*(np.pi/180))*turtleSize, -np.cos(direction*(np.pi/180))*turtleSize)
 
 def turn_left():
-    pass
-
+    global direction
+    direction -= 90
 
 def turn_right():
-    pass
+    global direction
+    direction += 90
 
-tk.Button(window, text="↑", command=move_up).grid(column=0, row=1)
-tk.Button(window, text="↓", command=move_down).grid(column=1, row=1)
-tk.Button(window, text="←", command=move_left).grid(column=2, row=1)
-tk.Button(window, text="→", command=move_right).grid(column=3, row=1)
-tk.Button(window, text="↰", command=turn_left).grid(column=1, row=2)
-tk.Button(window, text="↱", command=turn_left).grid(column=2, row=2)
+tk.Button(window, 
+          text="↰", 
+          height=7,
+          width=10,
+          command=turn_left).grid(column=0, row=1)
+tk.Button(window, 
+          text="↑",
+          height=7,
+          width=10,
+          command=move_forward).grid(column=1, row=1)
+tk.Button(window, 
+          text="↱", 
+          height=7,
+          width=10,
+          command=turn_right).grid(column=2, row=1)
+
 
 window.mainloop()
